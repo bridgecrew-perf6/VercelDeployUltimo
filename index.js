6da55c959354c5786763538c78c6c22a7372c48e
evisintin1 <evisintinmarquez@escuelasproa.edu.ar>
2022-06-06 15:51:41 -0300

    TestVercel v1.0.18

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ejs = require("ejs");
 const product = require("./api/product");
 
 var path = require ('path');
-app.use(express.static(path.join(__dirname + '../public')));
+app.set('views', path.join(__dirname, 'views'));
 
 app.set("view engine", "ejs");
 app.engine("ejs", require("ejs").__express);
